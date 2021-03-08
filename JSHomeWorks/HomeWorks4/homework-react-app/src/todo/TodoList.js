050227f3cf4ab57b8010f5910721c6540cfd85b7
mikiyur <mikiyur@gmail.com>
2021-03-08 15:13:27 +0200

    HW4 react.app was added

---
+++ JSHomeWorks/HomeWorks4/homework-react-app/src/todo/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react"
+import TodoItem from "./item";
+
+const style = {
+    listStyle: 'none',
+    margin: 0,
+    padding: 0
+}
+
+function TodoList(props) {
+    return (
+        <ul style={style}>
+            {props.todos.map(
+                (todo, index) => {
+                    return <TodoItem todo={todo} key={todo.id} index={index}/>
+                }
+            )}
+        </ul>
+    )
+}
+
+export default TodoList;
